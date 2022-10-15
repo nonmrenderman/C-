aca58d5071e1fc563df0a548d1b084ca6b01c3a4
nonmrenderman <80649804+nonmrenderman@users.noreply.github.com>
2022-10-15 16:47:17 -0400

    Add project files.

---
+++ file.cs
@@ -0,0 +1,19 @@
+public class Example
+{
+    public static void Main()
+    {
+        Console.Write("Hello ");
+        Console.WriteLine("World!");
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.Write("Good day, ");
+        Console.Write(name);
+        Console.WriteLine("!");
+    }
+}
+// The example displays output similar to the following:
+//       Hello World!
+//       Enter your name: James
+//       Good day, James!
+//(UNTESTED BY THE CSCC);
+//(TESTING WILL START ON: NOV 1st 2022 OR EAIRLIER);
